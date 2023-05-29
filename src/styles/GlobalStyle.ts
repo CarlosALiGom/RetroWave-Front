a595ad53fed707b7c4b525dd1f889c5357da64cb
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-29 19:34:20 +0200

    Add GlobalStyle resets and font and apply them to main

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  *,
+  ::after,
+  ::before {
+  box-sizing: border-box;
+  }
+  
+  html {
+    font-family: ${(prop) => prop.theme.font.primary};
+  }
+
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+
+  body {
+    background-color: ${(prop) => prop.theme.color.dark};
+    color: ${(prop) => prop.theme.color.light};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  button {
+    font-family: inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
