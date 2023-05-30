c30393e4233a6ae03b471156a67d3a578613bb43
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-30 10:06:13 +0200

    Refactor Header test with renderWithProviders

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,27 +1,13 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Header from "./Header";
-import { render, screen } from "@testing-library/react";
-import { ThemeProvider } from "styled-components";
-import theme from "../../styles/theme/theme";
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a Header component", () => {
   describe("When its rendered", () => {
     test("Then it should show the web logo with and alt text 'RetroWave logo'", () => {
       const expectedAlternativeText = "RetroWave logo";
-      const routes = [
-        {
-          path: "/",
-          element: <Header />,
-        },
-      ];
 
-      const router = createBrowserRouter(routes);
-
-      render(
-        <ThemeProvider theme={theme}>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      );
+      renderWithProviders(<Header />);
 
       const logo = screen.getByAltText(expectedAlternativeText);
 
