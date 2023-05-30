f9ea4cf1f81e19fbf92501f15ad843ebf67a0068
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-30 23:49:06 +0200

    Test LoginForm input values and submit

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -4,10 +4,14 @@ import { vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import renderWithProviders from "../../utils/testUtils";
 
+beforeAll(() => {
+  vi.clearAllMocks();
+});
 describe("Given a LoginForm component", () => {
   const submitForm = vi.fn();
   const usernameLabelText = "Username:";
   const passwordLabelText = "Password:";
+  const userDataText = "Frank";
   describe("When its rendered", () => {
     test("Then it should show a heading with the text 'Login'", () => {
       const expectedText = "Login";
@@ -45,8 +49,14 @@ describe("Given a LoginForm component", () => {
 
       const button = screen.getByRole("button");
 
-      await userEvent.type(screen.getByLabelText(usernameLabelText), "Frank");
-      await userEvent.type(screen.getByLabelText(passwordLabelText), "Frank");
+      await userEvent.type(
+        screen.getByLabelText(usernameLabelText),
+        userDataText
+      );
+      await userEvent.type(
+        screen.getByLabelText(passwordLabelText),
+        userDataText
+      );
 
       expect(button).toBeEnabled();
     });
@@ -59,6 +69,62 @@ describe("Given a LoginForm component", () => {
       const button = screen.getByRole("button");
 
       expect(button).toBeDisabled();
+    });
+  });
+
+  describe("When the username write 'Frank' on the username input", () => {
+    test("Then the input value must be 'Frank", async () => {
+      renderWithProviders(<LoginForm submitForm={submitForm} />);
+
+      await userEvent.type(
+        screen.getByLabelText(usernameLabelText),
+        userDataText
+      );
+
+      const usernameInputValue = screen.getByLabelText(usernameLabelText);
+
+      expect(usernameInputValue).toHaveValue(userDataText);
+    });
+  });
+
+  describe("When the username write 'Frank' on the password input", () => {
+    test("Then the input value must be 'Frank'", async () => {
+      renderWithProviders(<LoginForm submitForm={submitForm} />);
+
+      await userEvent.type(
+        screen.getByLabelText(passwordLabelText),
+        userDataText
+      );
+
+      const passwordInputValue = screen.getByLabelText(passwordLabelText);
+
+      expect(passwordInputValue).toHaveValue(userDataText);
+    });
+  });
+
+  describe("When the user writes 'Frank' username and 'Frank' password user credentials and clicks the button", () => {
+    test("Then the submit function should be called with the user credentials", async () => {
+      const userMock = {
+        username: userDataText,
+        password: userDataText,
+      };
+
+      renderWithProviders(<LoginForm submitForm={submitForm} />);
+
+      await userEvent.type(
+        screen.getByLabelText(usernameLabelText),
+        userDataText
+      );
+      await userEvent.type(
+        screen.getByLabelText(passwordLabelText),
+        userDataText
+      );
+
+      const button = screen.getByRole("button");
+
+      await userEvent.click(button);
+
+      expect(submitForm).toHaveBeenCalledWith(userMock);
     });
   });
 });
