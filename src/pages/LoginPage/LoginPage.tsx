a82042357099db9f35d0f9b784693d5aaeba34db
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-31 19:46:39 +0200

    Add onSubmit function on LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,24 @@
+import { useNavigate } from "react-router";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import useUser from "../../hooks/useUser/useUser";
+import { useAppDispatch } from "../../store";
+import { loginUserActionCreator } from "../../store/user/userSlice";
+import { UserCredentialsStructure } from "../../types";
+import useToken from "../../hooks/useToken/useToken";
 
 const LoginPage = (): React.ReactElement => {
-  const handleFormSubmit = (): void => {
-    return;
+  const { getUserToken } = useUser();
+  const { getTokenData } = useToken();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleFormSubmit = async (credentials: UserCredentialsStructure) => {
+    const token = await getUserToken(credentials);
+    if (token) {
+      const userData = getTokenData(token);
+      dispatch(loginUserActionCreator(userData));
+      navigate("/home", { replace: true });
+    }
   };
 
   return <LoginForm submitForm={handleFormSubmit} />;
