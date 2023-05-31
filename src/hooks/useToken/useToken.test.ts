2930b70c5a12a6266db2de4466ea7c93b11a758a
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-31 19:56:54 +0200

    Refactor useUser and useToken tests

--- src/hooks/useToken/useToken.test.ts
+++ src/hooks/useToken/useToken.test.ts
@@ -1,11 +1,18 @@
-import getTokenData from "./useToken";
 import { realTokenMock, userDataToken } from "../../mocks/userMocks";
+import useToken from "./useToken";
+import { renderHook } from "@testing-library/react";
 
 describe("Given a getTokenData function", () => {
   describe("When its called with a token", () => {
     test("Then it should return the token decoded", () => {
       const token = realTokenMock;
       const userData = userDataToken;
+
+      const {
+        result: {
+          current: { getTokenData },
+        },
+      } = renderHook(() => useToken());
 
       const tokenDecoded = getTokenData(token);
 
