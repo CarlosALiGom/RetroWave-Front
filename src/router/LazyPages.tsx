9e2477aa1ded2c74547f05bb44a451d026dd3409
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-04 16:29:20 +0200

    Add LazySynthPage to SynthPage test

--- src/router/LazyPages.tsx
+++ src/router/LazyPages.tsx
@@ -3,5 +3,5 @@ import { lazy } from "react";
 export const LazyLoginPage = lazy(() => import("../pages/LoginPage/LoginPage"));
 
 export const LazySynthsPage = lazy(
-  () => import("../pages/SynthsPage/SynthsPageStyled")
+  () => import("../pages/SynthsPage/SynthsPage")
 );
