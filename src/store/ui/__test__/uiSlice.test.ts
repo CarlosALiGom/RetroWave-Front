789f929da089e31a8a37fa1dc7c0c443507d75d7
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-04 21:48:08 +0200

    Test hideLoading reducer

--- src/store/ui/__test__/uiSlice.test.ts
+++ src/store/ui/__test__/uiSlice.test.ts
@@ -1,5 +1,6 @@
 import {
   UiState,
+  hideLoadingActionCreator,
   initialUiState,
   showLoadingActionCreator,
   uiReducer,
@@ -15,6 +16,20 @@ describe("Given a uiReducer", () => {
       const showLoadingAction = showLoadingActionCreator();
 
       const newUiState = uiReducer(initialUiState, showLoadingAction);
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+
+  describe("When it receives a current state and a hideLoading action", () => {
+    test("Then it should change the ui state property isLoading to false", () => {
+      const expectedUiState: UiState = {
+        isLoading: false,
+      };
+
+      const hideLoadingAction = hideLoadingActionCreator();
+
+      const newUiState = uiReducer(initialUiState, hideLoadingAction);
 
       expect(newUiState).toStrictEqual(expectedUiState);
     });
