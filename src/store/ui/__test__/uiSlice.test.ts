ae81cc64ccb529db337437f89af49651eb0d0fa1
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-05 12:11:36 +0200

    Test showError reducer

--- src/store/ui/__test__/uiSlice.test.ts
+++ src/store/ui/__test__/uiSlice.test.ts
@@ -2,6 +2,7 @@ import {
   UiState,
   hideLoadingActionCreator,
   initialUiState,
+  showErrorActionCreator,
   showLoadingActionCreator,
   uiReducer,
 } from "../uiSlice";
@@ -11,6 +12,8 @@ describe("Given a uiReducer", () => {
     test("Then it should change the ui state property isLoading to true", () => {
       const expectedUiState: UiState = {
         isLoading: true,
+        isError: false,
+        message: "",
       };
 
       const showLoadingAction = showLoadingActionCreator();
@@ -25,11 +28,34 @@ describe("Given a uiReducer", () => {
     test("Then it should change the ui state property isLoading to false", () => {
       const expectedUiState: UiState = {
         isLoading: false,
+        isError: false,
+        message: "",
       };
 
       const hideLoadingAction = hideLoadingActionCreator();
 
       const newUiState = uiReducer(initialUiState, hideLoadingAction);
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+
+  describe("When it receives a current state and a showError action with a message payload 'Error adding synth", () => {
+    test("Then it shoul change the ui state propert isError to true", () => {
+      const expectedUiState: UiState = {
+        isLoading: false,
+        isError: true,
+        message: "Error adding synth",
+      };
+
+      const errorMessage = "Error adding synth";
+
+      const showErrorAction = showErrorActionCreator({
+        message: errorMessage,
+        isError: true,
+      });
+
+      const newUiState = uiReducer(initialUiState, showErrorAction);
 
       expect(newUiState).toStrictEqual(expectedUiState);
     });
