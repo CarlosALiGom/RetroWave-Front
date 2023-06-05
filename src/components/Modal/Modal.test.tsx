3921bb671a727a467a1da9da6701925ed74efdd8
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-05 20:07:53 +0200

    Test Modal corner case when user close it

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -1,6 +1,13 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../utils/testUtils";
+import userEvent from "@testing-library/user-event";
 import Modal from "./Modal";
+import App from "../App/App";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
 
 describe("Given a Modal component", () => {
   describe("When its rendered", () => {
@@ -28,6 +35,25 @@ describe("Given a Modal component", () => {
       const button = screen.getByAltText(expectedAltText);
 
       expect(button).toBeInTheDocument();
+    });
+  });
+  describe("When its rendered in App and the user clicks the close button", () => {
+    test("Then the modal should disapear", async () => {
+      const expectedAltText = "Close button";
+
+      const routes: RouteObject[] = [{ path: "/", element: <App /> }];
+
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />, {
+        ui: { isError: true, message: "Error", isLoading: false },
+      });
+
+      const button = screen.getByAltText(expectedAltText);
+
+      await userEvent.click(button);
+
+      expect(button).not.toBeInTheDocument();
     });
   });
 });
