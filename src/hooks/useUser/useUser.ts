a4977f446d99a6eab42eb492874cf56585f87ccb
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-06 19:27:27 +0200

    Refactor useSynth and useUser

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -2,32 +2,40 @@ import axios from "axios";
 import { UserCredentialsStructure } from "../../types";
 import { errorMessages } from "../../utils/errorMessages";
 import { useAppDispatch } from "../../store";
-import { showErrorActionCreator } from "../../store/ui/uiSlice";
+import {
+  hideLoadingActionCreator,
+  showErrorActionCreator,
+} from "../../store/ui/uiSlice";
 import { apiUrl } from "../../mocks/handlers";
+import { useCallback } from "react";
 
 const useUser = () => {
   const dispatch = useAppDispatch();
-  const getUserToken = async (
-    credentials: UserCredentialsStructure
-  ): Promise<string | undefined> => {
-    try {
-      const {
-        data: { token },
-      } = await axios.post<{ token: string }>(
-        `${apiUrl}/user/login`,
-        credentials
-      );
-      return token;
-    } catch (error) {
-      (error as Error).message = errorMessages.wrongCredentials;
-      dispatch(
-        showErrorActionCreator({
-          message: errorMessages.wrongCredentials,
-          isError: true,
-        })
-      );
-    }
-  };
+  const getUserToken = useCallback(
+    async (
+      credentials: UserCredentialsStructure
+    ): Promise<string | undefined> => {
+      try {
+        const {
+          data: { token },
+        } = await axios.post<{ token: string }>(
+          `${apiUrl}/user/login`,
+          credentials
+        );
+        return token;
+      } catch (error) {
+        (error as Error).message = errorMessages.wrongCredentials;
+        dispatch(
+          showErrorActionCreator({
+            message: errorMessages.wrongCredentials,
+            isError: true,
+          })
+        );
+        dispatch(hideLoadingActionCreator());
+      }
+    },
+    [dispatch]
+  );
 
   return { getUserToken };
 };
