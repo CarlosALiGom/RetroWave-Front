11ded385a7ecc01f60f685f34a56187b80303a80
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-06 19:29:08 +0200

    Add conditional getSynths if isLogged on SynthsPage

--- src/hooks/useLocalStorage/useLocalStorage.ts
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -1,13 +1,11 @@
-import { useCallback } from "react";
-
 const useLocalStorage = () => {
   const setToken = (key: string, token: string) => {
     localStorage.setItem(key, token);
   };
 
-  const getToken = useCallback((key: string): string | null => {
+  const getToken = (key: string): string | null => {
     return localStorage.getItem(key);
-  }, []);
+  };
 
   const removeToken = (key: string) => {
     localStorage.removeItem(key);
--- src/pages/SynthsPage/SynthsPage.tsx
+++ src/pages/SynthsPage/SynthsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useAppDispatch } from "../../store";
+import { useAppDispatch, useAppSelector } from "../../store";
 import { loadSynthsActionCreator } from "../../store/synths/synthSlice";
 import SynthsPageStyled from "./SynthsPageStyled";
 import SynthList from "../../components/SynthList/SynthList";
@@ -8,13 +8,17 @@ import useSynths from "../../hooks/useSynths/useSynths";
 const SynthsPage = (): React.ReactElement => {
   const dispatch = useAppDispatch();
   const { getSynths } = useSynths();
+  const islogged = useAppSelector((state) => state.users.isLogged);
 
   useEffect(() => {
-    (async () => {
-      const synths = await getSynths();
-      if (synths) dispatch(loadSynthsActionCreator(synths));
-    })();
-  }, [dispatch, getSynths]);
+    islogged &&
+      (async () => {
+        const synths = await getSynths();
+        if (synths) {
+          dispatch(loadSynthsActionCreator(synths));
+        }
+      })();
+  }, [dispatch, getSynths, islogged]);
 
   return (
     <SynthsPageStyled className="header">
