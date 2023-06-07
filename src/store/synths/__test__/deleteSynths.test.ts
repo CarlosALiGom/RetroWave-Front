f76f5be00435d49fa0cb9916a023a9821caba8b5
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-07 13:08:52 +0200

    Test deleteSynths reducer

---
+++ src/store/synths/__test__/deleteSynths.test.ts
@@ -0,0 +1,29 @@
+import { expect } from "vitest";
+import {
+  SynthState,
+  deleteSynthActionCreator,
+  synthReducer,
+} from "../synthSlice";
+import { synthDbMocks } from "../../../mocks/synthsDbmocks";
+
+describe("Given a deleteSynths reducer", () => {
+  describe("When it receives a current synths state with 5 synths and a synth id payload that matches one of the synths", () => {
+    test("Then it should show the new state without the matched id payload one", () => {
+      const synthsList = synthDbMocks;
+
+      const currentSynthState: SynthState = {
+        synths: synthsList,
+      };
+
+      const expectedSynthState = {
+        synths: synthsList.slice(1),
+      };
+
+      const deleteSynthAction = deleteSynthActionCreator(synthsList[0].id);
+
+      const newSynthState = synthReducer(currentSynthState, deleteSynthAction);
+
+      expect(expectedSynthState).toStrictEqual(newSynthState);
+    });
+  });
+});
