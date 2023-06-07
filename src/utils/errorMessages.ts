1284f08547148bcc3aa67207d817f1ffe5f813a5
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-07 18:04:31 +0200

    Add deleteSynths function to useSynths custom hook

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -1,9 +1,11 @@
 interface ErrorMessagesStructure {
   wrongCredentials: string;
   synthsNotFound: string;
+  synthNotDeleted: string;
 }
 
 export const errorMessages: ErrorMessagesStructure = {
   wrongCredentials: "Wrong Credentials",
   synthsNotFound: "Sorry, synths can't be loaded",
+  synthNotDeleted: "Error deleting synth",
 };
---
+++ src/utils/feedbackMessages.ts
@@ -0,0 +1,7 @@
+interface FeedbackMessagesStructure {
+  synthDeleted: string;
+}
+
+export const feedbackMessage: FeedbackMessagesStructure = {
+  synthDeleted: "Synth deleted succesfully",
+};
