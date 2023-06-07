382dd23b14a19d88cea8b82c7619f0211e3bcc6b
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-07 10:01:19 +0200

    Test SynthPage redirects to LoginPage when user isn't logged

--- src/pages/SynthsPage/SynthsPage.test.tsx
+++ src/pages/SynthsPage/SynthsPage.test.tsx
@@ -4,6 +4,14 @@ import { LazySynthsPage } from "../../router/LazyPages";
 import SynthsPage from "./SynthsPage";
 import { UserDataState } from "../../mocks/userMocks";
 import { getSynthsDataMock } from "../../mocks/factories/synthFactory/synthFactory";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import paths from "../../router/paths/paths";
+import { initialUserState } from "../../store/user/userSlice";
+import LoginPage from "../LoginPage/LoginPage";
 
 describe("Given a SynthPage component", () => {
   describe("When its rendered", () => {
@@ -38,6 +46,30 @@ describe("Given a SynthPage component", () => {
       });
 
       const heading = await screen.getByRole("heading", { name: expectedText });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+
+  describe("When its rendered with the user not logged", () => {
+    test("Then it should redirect to login page and renders a LoginForm with a 'Login' heading", async () => {
+      const expectedHeading = "Login";
+
+      const routes: RouteObject[] = [
+        { path: paths.app, element: <SynthsPage /> },
+        { path: paths.login, element: <LoginPage /> },
+      ];
+
+      const router = createMemoryRouter(routes);
+
+      renderWithProviders(<RouterProvider router={router} />, {
+        users: initialUserState,
+      });
+
+      screen.debug();
+      const heading = await screen.getByRole("heading", {
+        name: expectedHeading,
+      });
 
       expect(heading).toBeInTheDocument();
     });
