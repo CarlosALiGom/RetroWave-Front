1120c3791842b56c4eb7cffa08b746d814a84e27
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-11 15:44:14 +0200

    Add Pagination component change pages functions by props

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,11 +1,41 @@
 import Button from "../Button/Button";
 import PaginationStyled from "./PaginationStyled";
 
-const Pagination = (): React.ReactElement => {
+interface PaginationProps {
+  skip: number;
+  totalSynths: number;
+  limit: number;
+  nextPage: () => void;
+  backPage: () => void;
+}
+const Pagination = ({
+  backPage,
+  nextPage,
+  skip,
+  totalSynths,
+  limit,
+}: PaginationProps): React.ReactElement => {
+  const nextPageOnClick = () => {
+    nextPage();
+  };
+  const backPageOnClick = () => {
+    backPage();
+  };
+
   return (
     <PaginationStyled className="pagination">
-      <Button className="pagination__button" text="Back" />
-      <Button className="pagination__button" text="Next" />
+      <Button
+        className="pagination__button"
+        text="Back"
+        actionOnClick={backPageOnClick}
+        isDisabled={skip === 0}
+      />
+      <Button
+        className="pagination__button"
+        text="Next"
+        actionOnClick={nextPageOnClick}
+        isDisabled={(skip + 1) * limit === totalSynths}
+      />
     </PaginationStyled>
   );
 };
