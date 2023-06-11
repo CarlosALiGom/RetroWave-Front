76f3176a71d1bd846743bc3c8b1da5a8344d9f20
Carlos Aliaga <carlosaligom@gmai.com>
2023-06-11 17:02:53 +0200

    Test pagination buttons on SynthPage

--- src/pages/SynthsPage/SynthsPage.test.tsx
+++ src/pages/SynthsPage/SynthsPage.test.tsx
@@ -12,6 +12,8 @@ import {
 import paths from "../../router/paths/paths";
 import userEvent from "@testing-library/user-event";
 import { synthDbMock } from "../../mocks/synthsDbmocks";
+import { server } from "../../mocks/server";
+import { handlers } from "../../mocks/handlers";
 
 describe("Given a SynthPage component", () => {
   describe("When its rendered", () => {
@@ -75,6 +77,34 @@ describe("Given a SynthPage component", () => {
       await userEvent.click(button);
 
       expect(heading).not.toBeInTheDocument();
+    });
+  });
+
+  describe("When its rendered with a list synths", () => {
+    test("Then if the user press next and then back to the first list, then the 'back' button should be disabled", async () => {
+      server.resetHandlers(...handlers);
+
+      const nextButtonText = "Next";
+      const backButtonText = "Back";
+
+      renderWithProviders(<SynthsPage />, {
+        users: {
+          id: "64707ddf2d09cd1540f0faaf",
+          name: "admin",
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYWRtaW4iLCJzdWIiOiI2NDcwN2RkZjJkMDljZDE1NDBmMGZhYWYiLCJpYXQiOjE2ODU1NDMwMjgsImV4cCI6MTY4NTgwMjIyOH0.8ow7vXeJ31asvveYlL6twB3khIvVPfNmteM5rZR_LjM",
+          isLogged: true,
+        },
+      });
+
+      const nextButton = screen.getByRole("button", { name: nextButtonText });
+      await userEvent.click(nextButton);
+      screen.debug();
+      const backButton = screen.getByRole("button", { name: backButtonText });
+      await userEvent.click(backButton);
+      screen.debug();
+      expect(nextButton).toBeEnabled();
+      expect(backButton).toBeDisabled();
     });
   });
 });
